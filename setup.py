814916de7ae937a0201f878b3dc497e21d6fea21
Samuel W. Failor <samuel.failor@gmail.com>
2023-04-14 22:25:46 +0100

    typo

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='srt4s2p',
     version='0.0.1',
-    author = 'Samuel W. Failor',
-    author_email = 'samuel.failor@gmail.com',
-    url = 'http://github.com/sfailor/srt4s2p',
+    author='Samuel W. Failor',
+    author_email='samuel.failor@gmail.com',
+    url='http://github.com/sfailor/srt4s2p',
     install_requires=   [
                             'pyqtgraph',
                             'PyQt5',
@@ -16,7 +16,7 @@ setup(
                             'scipy',
                             'matplotlib'                           
                         ],
-    packages = find_packages(),
+    packages=find_packages(),
     classifiers=[
                     "Programming Language :: Python :: 3",
                     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
