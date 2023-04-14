e165015ee79c3e90c18814013f51ca1074929224
Samuel W. Failor <samuel.failor@gmail.com>
2023-04-14 21:45:00 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='srt4s2p',
+    version='0.0.1',
+    author = 'Samuel W. Failor',
+    author_email = 'samuel.failor@gmail.com',
+    url = 'http://github.com/sfailor/srt4s2p',
+    install_requires=   [
+                            'pyqtgraph',
+                            'PyQt5',
+                            'key-point-finder',
+                            'numpy',
+                            'opencv-python',
+                            'pandas',
+                            'scipy',
+                            'matplotlib'                           
+                        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
+
